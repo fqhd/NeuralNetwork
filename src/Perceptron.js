ef9d986bd0dd8f347c645ddaf82135f574152c18
Fahd <fahd31256@gmail.com>
2021-04-15 12:34:33 +0000

    added perception train and learning points

--- src/Perceptron.js
+++ src/Perceptron.js
@@ -11,6 +11,7 @@ class Perceptron {
     //Initialize the weights randomly
     for (let i = 0; i < 2; i++) {
       this.weights = [];
+      this.learningRate = 0.1;
       this.weights.push(Math.random() * 2 - 1);
       this.weights.push(Math.random() * 2 - 1);
     }
@@ -23,6 +24,16 @@ class Perceptron {
     }
     let output = sign(sum);
     return output;
+  }
+
+  train(inputs, target){
+    let guess = this.guess(inputs);
+    let err = target - guess;
+
+    for(let i = 0; i < inputs.length; i++){
+      let deltaWeight = err * inputs[i];
+      this.weights[i] += deltaWeight * this.learningRate;;
+    }
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
 let perceptron = new Perceptron();
 let points = [];
+let guessPoints = [];
 
 function setup() {
   createCanvas(500, 500);
 
-  for(let i = 0; i < 100; i++){
+  // Adding the points
+  for(let i = 0; i < 1000; i++){
     points.push(new Point());
+  }
+
+  // Training our amazing little perceptron with a known set of points :)
+  for(p of points){
+    let inputs = [p.x, p.y];
+    perceptron.train(inputs, p.label);
+  }
+
+  // Classifying a set of points based on the guesses of our trained perceptron
+  for(let i = 0; i < 100; i++){
+    let p = new Point();
+    let inputs = [p.x, p.y];
+    p.label = perceptron.guess(inputs);
+    guessPoints.push(p);
   }
 
 }
@@ -13,7 +29,7 @@ function setup() {
 
 function draw() {
   background(200);
-  for(p of points){
+  for(p of guessPoints){
     p.draw();
   }
 }
