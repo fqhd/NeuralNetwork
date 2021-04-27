8b5c0e034c08d3e5a6fbf1b4c5c42f53b7917005
Fahd <fahd31256@gmail.com>
2021-04-27 12:56:39 +0000

    finished matrix library

--- src/Matrix.js
+++ src/Matrix.js
@@ -11,6 +11,37 @@ class Matrix {
       }
     }
   }
+
+  static multiply(m1, m2) {
+    if (m1.cols !== m2.rows) {
+      console.log("Columns of A must match rows of B");
+      return;
+    }
+    let output = new Matrix(m1.rows, m2.cols);
+    for (let i = 0; i < m1.rows; i++) {
+      for (let j = 0; j < m2.cols; j++) {
+        let product = 0;
+        for (let k = 0; k < m2.rows; k++) {
+          product += m1.matrix[i][k] * m2.matrix[k][j];
+        }
+        output.matrix[i][j] = product;
+      }
+    }
+    return output;
+  }
+
+}
+
+Matrix.prototype.map = function(func){
+  for(let i = 0; i < this.rows; i++){
+    for(let j = 0; j < this.cols; j++){
+      this.matrix[i][j] = func(this.matrix[i][j]);
+    }
+  }
+}
+
+Matrix.prototype.print = function() {
+  console.table(this.matrix);
 }
 
 Matrix.prototype.randomize = function() {
@@ -37,18 +68,20 @@ Matrix.prototype.add = function(n) {
   }
 }
 
-Matrix.prototype.multiply = function(n) {
-  if (n instanceof Matrix) {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this.matrix[i][j] *= n.matrix[i][j];
-      }
+Matrix.prototype.transpose = function() {
+  let output = new Matrix(this.cols, this.rows);
+  for (let i = 0; i < this.rows; i++) {
+    for (let j = 0; j < this.cols; j++) {
+      output.matrix[j][i] = this.matrix[i][j];
     }
-  } else {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this.matrix[i][j] *= n;
-      }
+  }
+  return output;
+}
+
+Matrix.prototype.multiply = function(n) {
+  for (let i = 0; i < this.rows; i++) {
+    for (let j = 0; j < this.cols; j++) {
+      this.matrix[i][j] *= n;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,26 +4,10 @@ let guessPoints = [];
 
 function setup() {
   createCanvas(500, 500);
+}
 
-  // Adding the points
-  for (let i = 0; i < 1000; i++) {
-    points.push(new Point());
-  }
-
-  // Training our amazing little perceptron with a known set of points :)
-  for (p of points) {
-    let inputs = [p.x, p.y, 1];
-    perceptron.train(inputs, p.label);
-  }
-
-  // Classifying a set of points based on the guesses of our trained perceptron
-  for (let i = 0; i < 100; i++) {
-    let p = new Point();
-    let inputs = [p.x, p.y];
-    p.label = perceptron.guess(inputs);
-    guessPoints.push(p);
-  }
-
+function func(n){
+  return n * 0.5;
 }
 
 
